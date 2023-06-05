f08d024eefbafe5d17dd92bb80ed9974c6cd0b1f
Jhon-Acevedo <jhon.acevedo01@uptc.edu.co>
2023-06-05 16:06:41 -0500

    Add login endpoint

--- apiGateway/main.py
+++ apiGateway/main.py
@@ -1,12 +1,42 @@
-from flask import Flask, jsonify
+import datetime
 import os
 
+import requests
+from flask import Flask, jsonify, request
+from flask.cli import load_dotenv
+from flask_cors import CORS
+from flask_jwt_extended import create_access_token, JWTManager
+
+load_dotenv()
+
 app = Flask(__name__)
+CORS(app)
+
+app.config["JWT_SECRET_KEY"]="super-secret"
+jwt = JWTManager(app)
+
+url_backend_security = os.getenv("URL_BACKEND_SECURITY")
 
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return jsonify({"Running Server ": True})
+
+
+@app.route('/login', methods=['POST'])
+def create_token():
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = url_backend_security + 'users/validate'
+    print(url)
+    response = requests.post(url, json=data, headers=headers)
+    if response.status_code == 200:
+        user = response.json()
+        expires = datetime.timedelta(seconds=60 * 60 * 24)
+        access_token = create_access_token(identity=user, expires_delta=expires)
+        return jsonify({"token": access_token, "user_id": user["id"]})
+    else:
+        return jsonify({"msg": "Bad username or password"}), 401
 
 
 if __name__ == '__main__':
