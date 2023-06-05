61a5930363e1968e3b5f5882d9afbb8d558a7014
keos14 <celis.kevin@gmail.com>
2023-06-05 18:40:18 -0500

    Update the SignUp Form for Users

--- frontend/src/components/SignUp.vue
+++ frontend/src/components/SignUp.vue
@@ -1,128 +1,135 @@
 <template>
   <div class="container">
-    <div class="col-md-6 offset-md-3 mt-5">
-      <br/>
-      <h1>Registrar usuario</h1>
+    <div class="col-md-8 offset-md-2 mt-4">
+      <h1 class="text-center mb-4">Registrar usuario</h1>
       <form v-on:submit.prevent="processSignUp">
-        <div class="form-group">
-          <label for="InputUsername">Username</label>
-          <input
-              type="text"
-              v-model="user.username"
-              class="form-control"
-              id="username"
-              placeholder="Username"
-              required="required"
-          />
-        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group mb-4">
+              <label for="InputNombre">Nombre</label>
+              <input
+                  type="text"
+                  v-model="user.nombre"
+                  class="form-control"
+                  id="nombre"
+                  placeholder="Nombre"
+                  required="required"
+              />
+            </div>  
 
-        <div class="form-group">
-          <label for="InputPassword">Password</label>
-          <input
-              type="password"
-              v-model="user.password"
-              class="form-control"
-              id="password"
-              placeholder="Username"
-              required="required"
-          />
-        </div>
+            <div class="form-group mb-4">
+              <label for="InputApellido">Apellido</label>
+              <input
+                  type="text"
+                  v-model="user.apellido"
+                  class="form-control"
+                  id="apellido"
+                  placeholder="Apellido"
+                  required="required"
+              />
+            </div>
 
-        <div class="form-group">
-          <label for="InputNombre">Nombre</label>
-          <input
-              type="text"
-              v-model="user.nombre"
-              class="form-control"
-              id="nombre"
-              placeholder="Nombre"
-              required="required"
-          />
-        </div>
+            <div class="form-group mb-4">
+              <label for="InputPassword">Contraseña</label>
+              <input
+                  type="password"
+                  v-model="user.password"
+                  class="form-control"
+                  id="password"
+                  placeholder="Contraseña"
+                  required="required"
+              />
+            </div>
 
+            <div class="form-group mb-4">
+              <label for="exampleInputName">Numero de documento</label>
+              <input
+                  type="text"
+                  v-model="user.numero_documento"
+                  class="form-control"
+                  id="numeroDocumento"
+                  placeholder="Numero de documento"
+                  required="required"
+              />
+            </div>
+          </div>
 
-        <div class="form-group">
-          <label for="InputApellido">Apellido</label>
-          <input
-              type="text"
-              v-model="user.apellido"
-              class="form-control"
-              id="apellido"
-              placeholder="Apellido"
-              required="required"
-          />
-        </div>
+          <div class="col-md-6">
+            <div class="form-group mb-4">
+              <label for="InputUsername" required="required">Nombre de usuario</label>
+              <input
+                  for="InputUsername"
+                  class="form-control"
+                  type="text"
+                  v-model="user.username"
+                  id="username"
+                  placeholder="Nombre de usuario"
+                  required="required"
+              />
+            </div>
+           
+            <div class="form-group mb-4">
+              <label for="InputEmail1" required="required">Correo Electronico</label>
+              <input
+                  type="email"
+                  v-model="user.email"
+                  class="form-control"
+                  id="email"
+                  aria-describedby="emailHelp"
+                  placeholder="Email"
+                  required="required"
+              />
+            </div>
 
-        <div class="form-group">
-          <label for="InputEmail1" required="required">Correo Electronico</label>
-          <input
-              type="email"
-              v-model="user.email"
-              class="form-control"
-              id="email"
-              aria-describedby="emailHelp"
-              placeholder="Email"
-              required="required"
-          />
-        </div>
+            <!--        Selecionar tipo de documento  obligaotrio-->
+            <div class="form-group mb-4">
+              <label for="InputTipoDocumento">Tipo de documento</label>
+              <select
+                  class="form-control"
+                  id="tipoDocumento"
+                  v-model="user.tipo_documento"
+                  required="required"
+              >
+                <option selected value="CC">Cedula de ciudadania</option>
+                <option value="TI">Tarjeta de identidad</option>
+                <option value="CE">Cedula de extranjeria</option>
+                <option value="RC">Registro civil</option>
+                <option value="PA">Pasaporte</option>
+              </select>
+            </div>
 
-        <!--        Selecionar tipo de documento  obligaotrio-->
-        <div class="form-group">
-          <label for="InputTipoDocumento">Tipo de documento</label>
-          <select
-              class="form-control"
-              id="tipoDocumento"
-              v-model="user.tipo_documento"
-              required="required"
-          >
-            <option selected value="CC">Cedula de ciudadania</option>
-            <option value="TI">Tarjeta de identidad</option>
-            <option value="CE">Cedula de extranjeria</option>
-            <option value="RC">Registro civil</option>
-            <option value="PA">Pasaporte</option>
-          </select>
-        </div>
-
-        <div class="form-group">
-          <label for="exampleInputName">Numero de documento</label>
-          <input
-              type="text"
-              v-model="user.numero_documento"
-              class="form-control"
-              id="numeroDocumento"
-              placeholder="Numero de documento"
-              required="required"
-          />
-        </div>
-
-        <div class="form-group">
-          <label for="InputDireccion">Direccion</label>
-          <input
-              type="text"
-              v-model="user.direccion"
-              class="form-control"
-              id="direccion"
-              placeholder="Direccion"
-              required="required"
-          />
-        </div>
-
-        <div class="form-group">
-          <label for="InputTelefono">Telefono</label>
-          <input
-              type="text"
-              v-model="user.telefono"
-              class="form-control"
-              id="telefono"
-              placeholder="Telefono"
-              required="required"
-          />
+           <div class="form-group mb-4">
+              <label for="InputDireccion">Direccion</label>
+              <input
+                  type="text"
+                  v-model="user.direccion"
+                  class="form-control"
+                  id="direccion"
+                  placeholder="Direccion"
+                  required="required"
+              />
+           </div>
+          </div>
+          <div class="form-group mb-4">
+              <label for="InputTelefono">Telefono</label>
+              <input
+                  type="text"
+                  v-model="user.telefono"
+                  class="form-control"
+                  id="telefono"
+                  placeholder="Telefono"
+                  required="required"
+              />
+            </div>
         </div>
         <hr/>
         <div class="success" v-if="isSuccess">
           We received your submission, thank you!
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+
+        <div class="col-md-6 offset-md-3 mt-5 text-center">
+          <button type="submit" class="btn btn-primary btn-lg btn-block">Registrar</button>
+        </div>
       </form>
     </div>
   </div>
