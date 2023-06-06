5e4b140edeb3b8e6a1821bc2944c71445ad2d8d2
keos14 <celis.kevin@gmail.com>
2023-06-06 09:17:26 -0500

    Update text in home page

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -12,12 +12,7 @@ import NavBar from "@/components/NavBar.vue";
   <!-- ======= About Section ======= -->
   <section id="about" class="about">
     <h3 class="text-center">Cow Shield</h3>
-    <p class="text-center"> Nuestra aplicación de control de bovinos es un
-      excelente
-      producto porque ofrece un seguimiento integral y fácil de usar para la gestión del ganado. Proporciona
-      funciones como registros de salud, seguimiento de peso y ubicación, y estadísticas detalladas. Nuestra
-      interfaz intuitiva y las características clave ayudan a los ganaderos a optimizar la productividad y el
-      bienestar del ganado.</p>
+    <p class="m-5" style="text-align: justify;">CowShield, se destaca como un producto excepcional gracias a su enfoque integral y fácil de usar. Diseñada específicamente para ganaderos, ofrece un seguimiento completo de las actividades relacionadas con el ganado. Desde registros de salud hasta seguimiento de peso y ubicación.</p>
     <div class="container" data-aos="fade-up">
       <div class="row gy-4">
         <div class="col-lg-6 position-relative about-img"
@@ -25,20 +20,15 @@ import NavBar from "@/components/NavBar.vue";
              data-aos-delay="150">
         </div>
         <div class="col-lg-5 d-flex align-items-end" data-aos="fade-up" data-aos-delay="300">
-          <div class="content ps-0 ps-lg-5">
-            <p class="fst-italic">
-              Con el control de vacunas puede llevar un mejor control de las vacunas que se le han aplicado a su
-              ganado.
+          <div class="content ps-0 ps-lg-4">
+            <p style="text-align: justify;">
+              Nuestra interfaz intuitiva y las características clave de CowShield están diseñadas para maximizar la productividad 
+              y el bienestar del ganado. Con el módulo de control de vacunas, los usuarios pueden mantener un seguimiento exhaustivo 
+              de las vacunas administradas a su ganado, garantizando una gestión sanitaria efectiva y segura.
             </p>
-            <ul>
-              <li><i class="bi bi-check2-all"></i> Aforador registra todo el ganado.</li>
-              <li><i class="bi bi-check2-all"></i> Administrador cuenta con dashboard para la gestion.</li>
-              <li><i class="bi bi-check2-all"></i> Puedes agregar vacunas y ver el historial de vacunacion.
-              </li>
-            </ul>
             <p>
-              CowShield es la mejor aplicación para la gestión de ganado, ya que cuenta con un sistema de control
-              de vacunas, aforador y administrador. Con CowShield puedes llevar un mejor control de tu ganado.
+              CowShield es la solución líder en gestión de ganado, gracias a su sistema de control de vacunas, su módulo de aforador 
+              y su completo panel de administración. Con CowShield, llevar un control efectivo de tu ganado nunca ha sido tan sencillo y confiable.
             </p>
 
             <div class="position-relative mt-1">
