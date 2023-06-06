b39326d4595e0795cfe5f81bdcb8673e6824ba1b
andres2202 <56946948+andres2202@users.noreply.github.com>
2023-06-05 21:43:22 -0500

    Update main.py

--- apiGateway/main.py
+++ apiGateway/main.py
@@ -90,6 +90,100 @@ def get_all_bovines():
     response = requests.get(url, headers=headers)
     return jsonify(response.json())
 
+@app.route('/bovine', methods=['POST'])
+def create_bovine():
+    url = url_backend_bovines + 'bovine'
+    data = request.get_json()
+    response = requests.post(url, headers=headers, json=data)
+    json = response.json()
+    return jsonify(json)
+
+@app.route('/bovine/<id>', methods=['GET'])
+def get_bovine_by_id(id):
+    url = url_backend_bovines + 'bovine/'+id
+    response = requests.get(url, headers=headers)
+    json = response.json()
+    return jsonify(json)
+
+@app.route('/bovine/<id>', methods=['DELETE'])
+def delete_bovine(id):
+    url = url_backend_bovines + 'bovine/'+id
+    response = requests.delete(url, headers=headers)
+    #json = response.json()
+    return jsonify(), response.status_code
+
+@app.route('/bovine/<id>', methods=['PUT'])
+def update_bovine(id):
+    data = request.get_json()
+    url = url_backend_bovines + 'bovine/'+id
+    response = requests.put(url, headers=headers, json=data)
+    json = response.json()
+    return jsonify(json)
+
+@app.route('/bovine/<id_bovine>/vaccine/<id_vaccine>', methods=['PUT'])
+def assign_vaccine(id_bovine, id_vaccine):
+    data = request.get_json()
+    url = url_backend_bovines + 'bovine/'+id_bovine + '/vaccine/' + id_vaccine
+    response = requests.put(url, headers=headers, json=data)
+    json = response.json()
+    return jsonify(json)
+
+@app.route('/bovine/vaccines/<id_bovine>', methods=['GET'])
+def get_list_vaccines(id_bovine):
+    url = url_backend_bovines + 'bovine/vaccines/'+id_bovine
+    response = requests.get(url, headers=headers)
+    json = response.json()
+    return jsonify(json)
+
+
+@app.route('/bovine/vaccines/<id_control>', methods=['DELETE'])
+def delete_bovine_vaccine(id_control):
+    url = url_backend_bovines + 'bovine/vaccines/' + id_control
+    response = requests.delete(url, headers=headers)
+    # json = response.json()
+    return jsonify(), response.status_code
+
+    
+
+
+
+    ###########Vaccines############
+
+@app.route('/vaccine/<id>', methods=['GET'])
+def get_vaccine_by_id(id):
+    url = url_backend_bovines + 'vaccine/'+id
+    response = requests.get(url, headers=headers)
+    return jsonify(response.json())
+
+@app.route('/vaccine', methods=['GET'])
+def get_all_vaccines():
+    url = url_backend_bovines + 'vaccine'
+    response = requests.get(url, headers=headers)
+    return jsonify(response.json())
+
+@app.route('/vaccine', methods=['POST'])
+def create_vaccine():
+    data = request.get_json()
+    url = url_backend_bovines + '/vaccine'
+    response = requests.post(url, headers=headers, json=data)
+    json = response.json()
+    return jsonify(json)
+
+@app.route('/vaccine/<id>', methods=['DELETE'])
+def delete_vaccine(id):
+    url = url_backend_bovines + '/vaccine/' + id
+    response = requests.delete(url, headers=headers)
+    # json = response.json()
+    return jsonify(), response.status_code
+
+@app.route('/vaccine/<id>', methods=['PUT'])
+def update_vaccine(id):
+    data = request.get_json()
+    url = url_backend_bovines + '/vaccine/' + id
+    response = requests.put(url, headers=headers, json=data)
+    json = response.json()
+    return jsonify(json)
+
 
 
 if __name__ == '__main__':
